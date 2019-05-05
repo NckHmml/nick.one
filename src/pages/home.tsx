f4b3ffa22b2ee1a0e3a327c8dca78073f9258195
Nick Hummel <NckHmml@users.noreply.github.com>
2019-05-05 14:27:44 +0200

    Added text blocks for homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
 
 export class HomePage extends React.Component {
   public render() {
     return (
       <>
         <Helmet>
-          <title>HomePage</title>
+          <title>Home</title>
         </Helmet>
 
         <div className="home-banner">
@@ -22,12 +22,49 @@ export class HomePage extends React.Component {
           </picture>
         </div>
 
-        <div className="g-24">
-          <h1>Hello World!</h1>
-          <h2>This is the HomePage</h2>
-          <Link to="/">Home</Link>
-          <br />
-          <Link to="/test">Test</Link>
+        <div className="home safe-area">
+          <div className="g-24">
+            <div className="g-24 g-md-16 g-lg-12">
+              <div className="home-item ">
+                <h1>About me</h1>
+                <p>
+                  I'm Nick Hummel, I have a passion for coding and solving problems.
+                  I am a creative, abstract and theoretical thinker and have the ability to turn ideas into solutions.
+                  I have extensive development experience with .NET technology and NodeJS be it either MVC or RESTful, I can also find my way around in Java, Scala and PHP.
+                  For frontend development I specialize in React with Typescript and also have some experience with Angular, but I find it easy to adapt myself to any other framework.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="g-24">
+            <div className="g-24 g-md-p-8 g-md-16 g-lg-p-12 g-lg-12">
+              <div className="home-item">
+                <h2 className="text-right">About this website</h2>
+                  <p>
+                    This website is a single page application (SPA) created with React, Typescript and server-sided rendering through NodeJS.
+                    The server-sided rendering is implemented for SEO optimization as the only search engine which crawl a SPA without any problems, is Google.
+                    And as we don't want to limit our exposure to just Google, we want our SPA to display content before the Javascript is executed on the browser.
+                    To reduce loading times, all pages are generated once, and then cached by Nginx.
+                  </p>
+                  <Link to="/about">Read more</Link>
+              </div>
+            </div>
+          </div>
+
+          <div className="g-24">
+            <div className="g-24 g-md-16 g-lg-12">
+              <div className="home-item ">
+                <h2 className="text-left">Kana learning tool</h2>
+                <p>
+                  I'm Nick Hummel, I have a passion for coding and solving problems.
+                  I am a creative, abstract and theoretical thinker and have the ability to turn ideas into solutions.
+                  I have extensive development experience with .NET technology and NodeJS be it either MVC or RESTful, I can also find my way around in Java, Scala and PHP.
+                  For frontend development I specialize in React with Typescript and also have some experience with Angular, but I can easily adapt to any other framework.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </>
     );
